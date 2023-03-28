69d200c998b509ae7916ee087ac4241ab24210fb
ro0k23 <125355361+ro0k23@users.noreply.github.com>
2023-03-28 10:19:02 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 basic.forever(function () {
     basic.showNumber(input.magneticForce(Dimension.Strength))
+    led.plotBarGraph(
+    input.magneticForce(Dimension.Strength),
+    2000
+    )
     basic.pause(100)
 })
